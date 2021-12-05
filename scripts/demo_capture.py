9eb34c6769f2fc00802ce4a276254191845dd1e7
Sebastian Spicker <sebastian.spicker@googlemail.com>
2021-12-05 17:39:00 +0100

    feat(core): ingest and normalize events from a single source

---
+++ scripts/demo_capture.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def build_core_summary() -> dict[str, str]:
+    return {"scope": "core", "status": "ready"}
+
+# current lane: core
+def core_task() -> dict[str, str]:
+    return {"scope": "core", "status": "ready"}
