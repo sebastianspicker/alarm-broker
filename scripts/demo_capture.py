478465941f1b417013175153a952b30724b636bc
Sebastian Spicker <sebastian.spicker@googlemail.com>
2022-07-10 16:27:00 +0200

    feat(dispatch): fan out normalized alarms to multiple sinks

--- scripts/demo_capture.py
+++ scripts/demo_capture.py
@@ -6,3 +6,7 @@ def build_core_summary() -> dict[str, str]:
 # current lane: core
 def core_task() -> dict[str, str]:
     return {"scope": "core", "status": "ready"}
+
+# current lane: dispatch
+def dispatch_task() -> dict[str, str]:
+    return {"scope": "dispatch", "status": "ready"}
