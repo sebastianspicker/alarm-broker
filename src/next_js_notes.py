69adc2b57e1f28ba3d70bbd3d71a2b60e2d0882a
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-11-21 19:30:00 +0100

    feat(next-js): wire the first app-router route structure for the alarm lane

---
+++ src/next_js_notes.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def build_next_js_summary() -> dict[str, str]:
+    return {"scope": "next js", "status": "ready"}
+
+# current lane: next_js
+def next_js_task() -> dict[str, str]:
+    return {"scope": "next js", "status": "ready"}
